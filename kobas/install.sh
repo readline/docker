d3ca86f86bb94ff78d98bba71c003c081acf3a4e
Kai Yu <finno@live.cn>
2016-07-10 19:25:41 +0800

    kobas install script modified since some link changed

--- kobas/install.sh
+++ kobas/install.sh
@@ -22,19 +22,20 @@ echo "biocLite(\"qvalue\")">>qvalue.R &&
 Rscript qvalue.R &&
 rm qvalue.R &&
 
-wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.2.30+-x64-linux.tar.gz &&
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.2.30/ncbi-blast-2.2.30+-x64-linux.tar.gz &&
 tar zxvf ncbi-blast-2.2.30+-x64-linux.tar.gz &&
 mv ncbi-blast-2.2.30+/bin/makeblastdb ncbi-blast-2.2.30+/bin/blastx ncbi-blast-2.2.30+/bin/blastp /bin/ &&
 rm -rf ncbi-blast-2.2.30+-x64-linux.tar.gz ncbi-blast-2.2.30+ &&
 
-wget http://kobas.cbi.pku.edu.cn/download/kobas2.0-20150126.tar.gz http://kobas.cbi.pku.edu.cn/download/sqlite3/organism.db.gz &&
-tar zxvf kobas2.0-20150126.tar.gz &&
-rm kobas2.0-20150126.tar.gz &&
-gunzip organism.db.gz &&
-mkdir kobas2.0-20150126/sqlite3 &&
-mv organism.db kobas2.0-20150126/sqlite3/ &&
-cp /opt/kobas2.0-20150126/docs/kobasrc /root/.kobasrc &&
-sed -i 's/kobas_home\ =/kobas_home \=\/opt\/kobas2\.0-20150126/g' ~/.kobasrc &&
+wget http://kobas.cbi.pku.edu.cn/download/kobas-2.1.1.tar.gz http://kobas.cbi.pku.edu.cn/download/sqlite3/organism.db.gz http://kobas.cbi.pku.edu.cn/download/sqlite3/ko.db.gz &&
+tar zxvf kobas-2.1.1.tar.gz &&
+rm kobas-2.1.1.tar.gz &&
+gunzip organism.db.gz ko.db.gz &&
+mv organism.db ko.db kobas-2.1.1/sqlite3/ &&
+cp /opt/kobas-2.1.1/doc/kobasrc /root/.kobasrc &&
+sed -i 's/kobas_home\ =/kobas_home \=\/opt\/kobas-2\.1\.1/g' ~/.kobasrc &&
 sed -i 's/blast_home\ =/blast_home \=\/bin/g' ~/.kobasrc &&
+export PATH=/opt/kobas-2.1.1/scripts/:$PATH
+export PYTHONPATH=/opt/kobas-2.1.1/src:$PYTHONPATH
 rm /opt/install.sh
 
